38c69f1ca004230177c546d4853b08ea93c7f3ee
mrymahmdlo <ahmdlomrym@gmail.com>
2024-01-29 18:43:27 +0330

    sale unit

---
+++ src/components/MySaleUnit/SaleUnitCard.tsx
@@ -0,0 +1,187 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Divider } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Skeleton from '@mui/material/Skeleton';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import { activityType, unitType } from '../SaleUnit/saleUnitEnum';
+
+interface MediaProps {
+  loading?: boolean;
+  data?: {
+    name: string;
+    unitType: string;
+    activityType: string;
+    address: string;
+    bio: string;
+    phoneNumbers: string[];
+    saleUnitDocuments: {
+      active: boolean;
+      attachmentId: number;
+      saleUnitDocumentType: string;
+    }[];
+  };
+}
+
+const getUnitTypeLabel = (value: string) => {
+  const unitTypeObject = unitType.find((type) => type.value === value);
+  return unitTypeObject ? unitTypeObject.label : '';
+};
+
+const getActivityTypeLabel = (value: string) => {
+  const unitTypeObject = activityType.find((type) => type.value === value);
+  return unitTypeObject ? unitTypeObject.label : '';
+};
+
+export default function Media(props: MediaProps) {
+  const { loading, data } = props;
+  const [defaultFile, setDefaultFile] = React.useState<File | null>(null);
+  const [defaultPic, setDefaultPic] = React.useState<File | null>(null);
+
+  React.useEffect(() => {
+    if (data?.saleUnitDocuments) {
+      let attachmentAvatarId: number;
+      let attachmentNoId: number;
+
+      const fetchImageAvatar = async () => {
+        data.saleUnitDocuments
+          .filter((k) => k.saleUnitDocumentType === 'PROFILE')
+          .map((doc) => (attachmentAvatarId = doc.attachmentId));
+        try {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_SERVER_URL}/file/getFile?attachmentId=${attachmentAvatarId}`
+          );
+          if (response.ok) {
+            const blob = await response.blob();
+            setDefaultFile(new File([blob], 'filename'));
+          } else {
+            console.error('Failed to fetch image:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error fetching image:', error);
+        }
+      };
+      const fetchImageNo = async () => {
+        data.saleUnitDocuments
+          .filter((k) => k.saleUnitDocumentType !== 'PROFILE')
+          .map((doc) => (attachmentNoId = doc.attachmentId));
+        try {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_SERVER_URL}/file/getFile?attachmentId=${attachmentNoId}`
+          );
+          if (response.ok) {
+            const blob = await response.blob();
+            setDefaultPic(new File([blob], 'filename'));
+          } else {
+            console.error('Failed to fetch image:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error fetching image:', error);
+        }
+      };
+      fetchImageAvatar();
+      fetchImageNo();
+    }
+  }, [data]);
+
+  return (
+    <Card style={{ width: 'inherit' }} sx={{ maxWidth: 345, m: 2 }}>
+      <CardHeader
+        avatar={
+          loading ? (
+            <Skeleton
+              animation='wave'
+              variant='circular'
+              width={40}
+              height={40}
+            />
+          ) : (
+            <Avatar
+              alt='avatar'
+              style={{ marginLeft: '0.5rem' }}
+              src={defaultFile ? URL.createObjectURL(defaultFile) : ''}
+            />
+          )
+        }
+        action={
+          loading ? null : (
+            <IconButton aria-label='settings'>
+              <MoreVertIcon />
+            </IconButton>
+          )
+        }
+        title={
+          loading ? (
+            <Skeleton
+              animation='wave'
+              height={10}
+              width='80%'
+              style={{ marginBottom: 6 }}
+            />
+          ) : (
+            data?.name
+          )
+        }
+        subheader={
+          loading ? (
+            <Skeleton animation='wave' height={10} width='40%' />
+          ) : (
+            <div style={{ color: '#1da1f3' }}>
+              {data?.unitType && getUnitTypeLabel(data?.unitType)}
+              <br />
+              {data?.activityType && getActivityTypeLabel(data?.activityType)}
+            </div>
+          )
+        }
+      />
+      {loading ? (
+        <Skeleton sx={{ height: 190 }} animation='wave' variant='rectangular' />
+      ) : (
+        <CardMedia
+          component='img'
+          height='140'
+          image={defaultPic ? URL.createObjectURL(defaultPic) : ''}
+        />
+      )}
+      <CardContent>
+        {loading ? (
+          <React.Fragment>
+            <Skeleton
+              animation='wave'
+              height={10}
+              style={{ marginBottom: 6 }}
+            />
+            <Skeleton animation='wave' height={10} width='80%' />
+          </React.Fragment>
+        ) : (
+          <>
+            <Typography variant='body2' color='text.secondary' component='p'>
+              {data?.bio}
+            </Typography>
+            <Divider />
+            <Typography fontSize={'small'} color='#1da1f3'>
+              آدرس
+            </Typography>
+            <Typography variant='body2' color='text.secondary' component='p'>
+              {data?.address}
+            </Typography>
+            <Divider />
+            <Typography fontSize={'small'} color='#1da1f3'>
+              شماره تماس
+            </Typography>
+            <Typography variant='body2' color='text.secondary' component='p'>
+              |{data?.phoneNumbers.map((k) => k + '|')}
+            </Typography>
+          </>
+        )}
+      </CardContent>
+    </Card>
+  );
+}
--- src/components/MySaleUnit/UserSaleUnits.tsx
+++ src/components/MySaleUnit/UserSaleUnits.tsx
@@ -2,17 +2,23 @@
 import { useAppDispatch } from '@/context';
 import { showSnackbar } from '@/context/slices/snackbarSlice';
 import getWithToken from '@/utils/getWithToken';
+import { Grid } from '@mui/material';
 import { useTranslations } from 'next-intl';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Media from './SaleUnitCard';
+// import SaleUnitCard from './SaleUnitCard';
 
 export default function UserSaleUnits() {
   const dispatch = useAppDispatch();
   const t = useTranslations();
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     getWithToken('/saleUnit/user?page=0&size=10', 'GET')
       .then((res: any) => {
-        console.log('object', res);
+        console.log('object', res.content);
+        setData(res.content);
       })
       .catch((err: { message: any }) => {
         if (err.message) {
@@ -30,8 +36,19 @@ export default function UserSaleUnits() {
             })
           );
         }
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
-  return <>saxd</>;
+  return (
+    <Grid container display={'flex'}>
+      {loading ? (
+        <Media loading={loading} />
+      ) : (
+        data.map((k, index) => <Media key={index} loading={loading} data={k} />)
+      )}
+    </Grid>
+  );
 }
